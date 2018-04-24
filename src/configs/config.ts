5fb4a0d5832582a5eeaff6eaf5d2772ddd8ea6bb
Timbo <5blaesch@informatik.uni-hamburg.de>
2018-04-24 12:58:05 +0200

    Adds a file with all configurable varibles of the server

---
+++ src/configs/config.ts
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT || 3001;
+export const LOG_LEVEL = process.env.LOG_LEVEL || "debug";
+export const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
