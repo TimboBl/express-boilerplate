d6f9ab466eae65cd4c780fb81150eb601e9e2316
Timbo <5blaesch@informatik.uni-hamburg.de>
2018-04-24 14:09:12 +0200

    Adds handling for no mongo connection string

--- src/services/MongoService.ts
+++ src/services/MongoService.ts
@@ -4,22 +4,26 @@ import * as Bluebird from "bluebird";
 import { logger } from "../utils/logger";
 import { MongoServiceT } from "../types/services/MongoServiceT";
 
-export const MongoService = ((): {connect: Function, shutdown: Function} => {
+export const MongoService = ((): {connect(): Bluebird<MongoServiceT>, shutdown(): Bluebird<void>} => {
     (<any>mongoose).Promise = Bluebird;
 
     // Implement your queries here and add them to the mongoService object
 
-    const mongoService = {};
+    const mongoService: MongoServiceT = {};
 
     const connect = (): Bluebird<MongoServiceT> => {
         logger.debug("Connecting to mongoDB", {connectionString: MONGO_CONNECTION_STRING});
-        if (!MONGO_CONNECTION_STRING) logger.info("You need to set the Mongo Connection string in your env variables!");
+        if (!MONGO_CONNECTION_STRING) {
+            logger.info("You need to set the Mongo Connection string in your env variables!");
+            return Bluebird.reject("You have not provided a connection string in the env properties");
+        }
+        mongoose.connection.on("error", (err: Error) => logger.error("There was an error connection to mongoDB", {error: err}));
         return mongoose.connect(MONGO_CONNECTION_STRING).then(() => {
             return mongoService;
         });
     };
 
-    const shutdown = (): Bluebird<any> => {
+    const shutdown = (): Bluebird<void> => {
         return mongoose.connection.close();
     };
 
@@ -27,4 +31,4 @@ export const MongoService = ((): {connect: Function, shutdown: Function} => {
         connect,
         shutdown,
     };
-});
+})();
