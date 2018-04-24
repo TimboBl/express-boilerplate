3ead0948b7b8e512d84404290ac7dc531a1e9e13
Timbo <5blaesch@informatik.uni-hamburg.de>
2018-04-24 12:43:23 +0200

    Adds error handling middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,52 @@
 import * as express from "express";
+import * as cors from "cors";
 import * as Bluebird from "bluebird";
 import { PORT } from "./configs/config";
 import * as http from "http";
+import { logger } from "./utils/logger";
+import { AppT } from "./types/App";
 
-export const App = (() => {
+export const App = ((): AppT => {
     const app = express();
     let server: http.Server;
+
+    app.use((req: express.Request, res: express.Response, next: Function) => {
+        logger.info("New Request: ", {
+            method: req.method,
+            url: req.url,
+        });
+        req.headers["Content-Type"] = "application/json";
+        next();
+    });
+
+    // Instantiate your middleware here
+    app.use(express.json()); // Parse the body of the request as JSON
+    app.use(express.urlencoded({extended: true})); // Parse all URL encoded parameters
+    app.use(cors()); // Set Allow Access Origin Headers
+
+    // Catch any errors that happened during the parsing process
+    app.use((err: Error, req: express.Request, res: express.Response, next: Function) => {
+       if (err) {
+           logger.error("Parsing error on incoming request!", {error: err});
+           res.status(422/*Unprocessable Entity*/)
+               .send({message: "The parameters you supplied are not compatible with this API"});
+           return;
+       }
+       next();
+    });
+
+    // Your routes go here!
+
+    // This route sends a 404 if non of your routes matched
+
+    app.all("*", (req: express.Request, res: express.Response) => {
+        logger.info("Request to non recognizes route", {
+            method: req.method,
+            url: req.url,
+
+        });
+        res.status(404).send({message: "Not found"});
+    });
 
     const listen = () => {
         server = app.listen(PORT, (err: Error) => {
